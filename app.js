bb30e608cafe54d475cfa0e0378d2e72b67a5d96
chiesap-db <chiesap@donboscobariloche.edu.ar>
2026-04-01 10:20:51 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Base de datos de las canciones (Añade el resto siguiendo este formato)
 const songs = [
     {
-        title: "1. Dulce Doncella",
+        title: "Dulce Doncella",
         content: `[DO]            [MI7]       [LAm]
 Estamos vivos y vivimos
         [FA]            [SOL]
@@ -26,7 +26,7 @@ hoy hay flores de pureza
 porque hacia ti caminamos espéranos`
     },
     {
-        title: "2. Gloria a Dios",
+        title: "Gloria a Dios",
         content: `[SOL]            [RE] [SOL]
 Gloria a Dios en el cielo
 [DO] [RE] [SOL] [DO] [RE] [SOL]
@@ -42,7 +42,7 @@ Y por salvar al mundo de su pecado
 enviase desde el cielo a tu hijo amado.`
     },
     {
-        title: "2b. Gloria a Dios, Gloria al Padre",
+        title: "Gloria a Dios, Gloria al Padre",
         content: `Gloria a Dios, Gloria a Dios,
 Gloria al Padre.
 A Él que sea la gloria.
@@ -59,7 +59,7 @@ A Él que sea la Gloria
 Aleluya amén.`
     },
     {
-        title: "3. Aleluya",
+        title: "Aleluya",
         content: `[DO]
 ¡Aleluya, gloria Aleluya!
 [LAm]
@@ -85,7 +85,7 @@ Que animas a tu iglesia, con tu aliento
 para que anuncie al mundo, el evangelio.`
     },
     {
-        title: "4. Quiero ser Pan",
+        title: "Quiero ser Pan",
         content: `[RE]             [LA]             [SIm]      [FA#m]
 Es joven el que espera, el que sabe caminar,
 [SOL]                       [RE]             [MI]         [LA]
@@ -113,7 +113,7 @@ El que vive siempre abierto, el que sabe transformar,
 El que canta con los otros, defendiendo la verdad.`
     },
     {
-        title: "5. Santo",
+        title: "Santo",
         content: `[DO]    [FA]    [DO]    [SOL]
 Santo, Santo, Santo es el Señor
 [DO]    [FA]    [DO]    [SOL] [DO] [SOL] [DO]
@@ -130,7 +130,7 @@ Hosana a Él, Hosana a Él,
 Hosana Cristo el Señor.`
     },
     {
-        title: "6. Padre nuestro en ti creemos",
+        title: "Padre nuestro en ti creemos",
         content: `[LA]                       [FA#m]
 En el mar he oído hoy, Señor tu voz que me llamó
 [RE]                                    [MI]
@@ -155,7 +155,7 @@ Pero se que si algún día por enseñar tu verdad
 a mi hermano y junto al él yo repetirte:`
     },
     {
-        title: "7. Cordero",
+        title: "Cordero",
         content: `[REm]             [DO]              [REm]      [REm]             [DO]              [REm]
 Cordero de Dios, Cordero de Dios, 
 [FA]                                     [DO]           [FA]
@@ -180,7 +180,7 @@ Bendito es el que viene
 en el nombre del Señor (x2)`
     },
     {
-        title: "8. Vida en abundancia",
+        title: "Vida en abundancia",
         content: `[MI]                                              [LA]                      [SI7]
 Los lirios del campo y las aves del cielo
 [LA]                                                       [MI]                     [SI7]
@@ -204,7 +204,7 @@ Vida en abundancia, en abundancia
 Sentite vivo la fiesta del reino comienza acá`
     },
     {
-        title: "9. Esto que soy (arranque escalonado)",
+        title: "Esto que soy (arranque escalonado)",
         content: `[MI]
 A veces me pregunto ¿porqué yo?,
 [DO#m]
@@ -239,7 +239,7 @@ y todas las personas que me diste
 desde mi corazón te las ofrezco`
     },
     {
-        title: "10. Auxiliadora que linda eres",
+        title: "Auxiliadora que linda eres",
         content: `[MIm]
 Para celebrar tu día
 [LAm]
@@ -265,7 +265,7 @@ porque con fuerza infinita,
 revivió mi corazón.`
     },
     {
-        title: "11. Rendid a Yahvé",
+        title: "Rendid a Yahvé",
         content: `[MI]                   [LA]
 Rendid a Yahvé santos del Señor
 [MI]                                                 [SI7]
@@ -296,7 +296,7 @@ La fuerza da al que pone su confianza en Él
 Yahvé bendice a su pueblo con paz`
     },
     {
-        title: "12. Mar adentro",
+        title: "Mar adentro",
         content: `[MI]                      [LA]
 Es hora de partir
 [MI]                               [SI7]
@@ -332,7 +332,7 @@ quiero llevar tu luz
 a ese mundo que no habla de Dios.`
     },
     {
-        title: "13. Candombe del Oratorio",
+        title: "Candombe del Oratorio",
         content: `[LAm]                            [MI]             [MI7]            [LAm]
 Venimos de todas partes, queremos vivir alegres,
 [LA7]                            [LAm]                                [REm]
@@ -369,7 +369,7 @@ y hace cien años
 que cada día nos quiere más`
     },
     {
-        title: "14. Tú nos transformas, Señor",
+        title: "Tú nos transformas, Señor",
         content: `[LA]                                      [MI]
 Te ofrecemos nuestras obras,
 [FA#m]                     [RE]
@@ -397,7 +397,7 @@ que da la vida también,
 y así poder entregarme como Tú.`
     },
     {
-        title: "15. Esperanza",
+        title: "Esperanza",
         content: `[SOL]       [RE]               [MIm]            [SIm]
 Hoy al fin tenemos que seguir,
 [DO]
@@ -441,7 +441,7 @@ no nos dejarás,
 esperamos contra toda esperanza.`
     },
     {
-        title: "16. El Dios de la Vida",
+        title: "El Dios de la Vida",
         content: `[DO]                               [SOL]
 Somos un nuevo pueblo,
 [LAm]                             [MIm]
@@ -485,7 +485,7 @@ y rechazamos toda idolatría,
 sólo creemos en el Dios de la Vida.`
     },
     {
-        title: "17. Secuencia de Pascua",
+        title: "Secuencia de Pascua",
         content: `[DO]                   [REm7]
 Ofrezcan los cristianos
 [FA]                       [DO]
@@ -541,7 +541,7 @@ en tu victoria santa.
 ¡Y Aleluya, Aleluya...`
     },
     {
-        title: "18. Eso que tu me das",
+        title: "Eso que tu me das",
         content: `Intro: [DO] [FA7] [DO] [SOL7]
 
 [DO]                      [SOL]
@@ -631,7 +631,7 @@ es mucho más
 de lo que nunca he merecido.`
     },
     {
-        title: "19. Alma misionera",
+        title: "Alma misionera",
         content: `[MI]             [SI7]          [DO#m]
 Señor, toma mi vida nueva,
 [LA]                             [MI]
@@ -673,7 +673,7 @@ condúceme a la tierra
 que tenga sed de Dios.`
     },
     {
-        title: "20. Ven y danos vida",
+        title: "Ven y danos vida",
         content: `[DO] [REm] [SOL] [FA] [DO] [REm] [SOL] [FA]
 
 [DO]                [REm]          [SOL]          [FA]
@@ -712,7 +712,7 @@ Queremos saciar el hambre
 que tienen todos los hombres de amar.`
     },
     {
-        title: "21. Lo que agrada a Dios",
+        title: "Lo que agrada a Dios",
         content: `INTRO: [DO]...[LAm]... [FA]...[SOL]...[DO]...
 
 [DO]                        [LAm]                           [(RE-SIm)]
@@ -727,7 +727,7 @@ que ten...go en su misericordia
 [Bis] [(1LA7)]`
     },
     {
-        title: "22. Emaús",
+        title: "Emaús",
         content: `[D]                 [Bm]             [A]             [G]
 Aquí vamos Jesús, caminando con Vos,
 dando un paso, queriéndote elegir.
@@ -775,7 +775,7 @@ danos tu paz,// (X2, segunda vez solo percu)
 danos tu paz (danos de tu paz...)`
     },
     {
-        title: "23. Haciéndote pan",
+        title: "Haciéndote pan",
         content: `[FA#m]                             [LA]         [MI]
 Comenzaste a hacerte pan en Belén,
 [RE]                                    [DO#m]
@@ -825,7 +825,7 @@ y tu entrega Pan Jesús
 fue más fuerte que el madero.`
     },
     {
-        title: "24. El plan de la mariposa",
+        title: "El plan de la mariposa",
         content: `Intro: [SOLm] | [DO] | [FA] | [SIb]
 
 [SOLm]                    [DO]
